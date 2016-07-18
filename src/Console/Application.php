ef24ac9f194014ec7c5eb8802bf7a17396806f30
Alexey Karapetov <f3ath@users.noreply.github.com>
2016-07-17 21:24:50 -0700

    Release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends ConsoleApplication
 {
-    const VERSION = '0.0.0';
+    const VERSION = '0.0.5';
 
     const DEFAULT_CONFIG_FILE = 'migrator.php';
 
